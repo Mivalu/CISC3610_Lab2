3194b1a4564d38b649094a92a8ac2f83eb96af8e
Michele Lugo <peachyjello@gmail.com>
2017-04-03 21:23:18 -0400

    lab 2 uploaded

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-function showInput() {
-	concatString = document.getElementById("user_input").value + " " + 
-		document.getElementById("user_input1").value + " " + 
-		document.getElementById("user_input2").value + " " + 
-		document.getElementById("user_input3").value + " " + 
-		document.getElementById("user_input4").value;
-        document.getElementById('display').innerHTML = concatString;
+
+
+var synth = window.speechSynthesis;
+
+var inputForm = document.querySelector('form');
+var read1 = document.getElementById('name');
+var read2 = document.getElementById('number');
+var read3 = document.getElementById('noun');
+var read4 = document.getElementById('verb');
+var read5 = document.getElementById('animal');
+var read6 = document.getElementById('name2');
+
+var options = document.getElementsByName('voice');
+var num = document.getElementById('voice').value;
+
+var voices = [];
+voices = synth.getVoices();
+
+
+inputForm.onsubmit = function(event) {
+  event.preventDefault();
+
+  var utterThis = new SpeechSynthesisUtterance(read1.value + ' is my name and I am ' + read2.value + 
+  	' years old. I like to eat ' + read3.value + ' and to ' + read4.value + '. I have a ' + read5.value + ' named ' + read6.value);
+
+  for (var i=0;i<voices.length;i++){
+  	if (options[i].checked){
+  	  	utterThis.voice = voices[parseInt(options[i].value)];
+  	  	break;
+  	  }
+  }
+  
+  synth.speak(utterThis);
+
+
+  read1.blur();
 }
